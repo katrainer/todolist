bf63b6e1b934b7e96fafffd0da94bc3107b11269
katrainer <laub94@list.ru>
2021-10-16 19:18:34 +0300

    Updates

---
+++ precache-manifest.eae15a859bad7d545a5b344f7b93092f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe9ffbe2408313663b0258202390f1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "443746920c48f893d7bc",
+    "url": "/static/css/main.79e7abbf.chunk.css"
+  },
+  {
+    "revision": "35052b1e1d135458955e",
+    "url": "/static/js/2.39748245.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.39748245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "443746920c48f893d7bc",
+    "url": "/static/js/main.359c4236.chunk.js"
+  },
+  {
+    "revision": "b201489dcbcc9f77b2de",
+    "url": "/static/js/runtime-main.e19410a0.js"
+  }
+]);
